02ed5dcee08dc965bc0469cf47b103e79fae168a
farimarwat <farimarwat@gmail.com>
2025-02-20 12:50:52 +0500

    Fix memory leak in FfmpegStreamExtractor

--- library/src/main/java/com/farimarwat/ffmpeg/FfmpegStreamExtractor.kt
+++ library/src/main/java/com/farimarwat/ffmpeg/FfmpegStreamExtractor.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import java.io.BufferedReader
 import java.io.File
 import java.io.FileInputStream
@@ -13,17 +14,19 @@ import java.lang.Thread.sleep
 
 object FfmpegStreamExtractor{
     private var mFfmpegPid = 0
-
+    private var shouldStop = false
      fun readStream(process:Process, progressCallBack: ((Float, Long, String) -> Unit)? = null):Job =
          CoroutineScope(Dispatchers.IO).launch {
         try {
             val pythonPID = ProcessUtils.getPythonProcessId(process)
             var line: String?
-            while (true) {
+            var ffmpegInitiated = false
+            while (!shouldStop) {
                 mFfmpegPid = ProcessUtils.getFFMPEGProcessId(pythonPID)
                 val progressFilePath = "/proc/$mFfmpegPid/fd/2"
                 val progressFile = File(progressFilePath)
                 if (progressFile.exists()) {
+                    ffmpegInitiated = true
                     val inputStream = FileInputStream(progressFile)
                     val reader = BufferedReader(InputStreamReader(inputStream))
                     while (reader.readLine().also { line = it } != null) {
@@ -32,6 +35,9 @@ object FfmpegStreamExtractor{
                     }
                 }
                 sleep(1000)
+                if(ffmpegInitiated && mFfmpegPid == -1){
+                    shouldStop = true
+                }
             }
         } catch (ex: Exception) {
             ex.printStackTrace()
@@ -43,6 +49,7 @@ object FfmpegStreamExtractor{
         val ffmpegid = ProcessUtils.getFFMPEGProcessId(pythonPID)
         ProcessUtils.killProcess(ffmpegid)
         process.destroy()
+        shouldStop = true
     }
     fun hasFfmpegProcess(process: Process):Boolean{
         val pythonPID = ProcessUtils.getPythonProcessId(process)
