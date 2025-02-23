cac1e8bf5c5cdae8ac377e5dfa3e76170c935838
farimarwat <farimarwat@gmail.com>
2025-02-23 12:12:22 +0500

    Convert VideoFormat class to data class

--- library/src/main/java/com/farimarwat/library/mapper/VideoFormat.kt
+++ library/src/main/java/com/farimarwat/library/mapper/VideoFormat.kt
@@ -4,35 +4,35 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-class VideoFormat {
-    val asr = 0
-    val tbr = 0
-    val abr = 0
-    val format: String? = null
+data class VideoFormat (
+    val asr:Int = 0,
+    val tbr:Int = 0,
+    val abr:Int = 0,
+    val format: String? = null,
 
     @JsonProperty("format_id")
-    val formatId: String? = null
+    val formatId: String? = null,
 
     @JsonProperty("format_note")
-    val formatNote: String? = null
-    val ext: String? = null
-    val preference = 0
-    val vcodec: String? = null
-    val acodec: String? = null
-    val width = 0
-    val height = 0
+    val formatNote: String? = null,
+    val ext: String? = null,
+    val preference:Int = 0,
+    val vcodec: String? = null,
+    val acodec: String? = null,
+    val width:Int = 0,
+    val height:Int = 0,
 
     @JsonProperty("filesize")
-    val fileSize: Long = 0
+    val fileSize: Long = 0,
 
     @JsonProperty("filesize_approx")
-    val fileSizeApproximate: Long = 0
-    val fps = 0
-    val url: String? = null
+    val fileSizeApproximate: Long = 0,
+    val fps:Int = 0,
+    val url: String? = null,
 
     @JsonProperty("manifest_url")
-    val manifestUrl: String? = null
+    val manifestUrl: String? = null,
 
     @JsonProperty("http_headers")
-    val httpHeaders: Map<String, String>? = null
-}
+    val httpHeaders: Map<String, String>? = null,
+)
