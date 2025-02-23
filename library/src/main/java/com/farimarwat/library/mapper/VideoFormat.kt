de86e27474f88e4879ec7ff28df7040d29cb46ba
farimarwat <farimarwat@gmail.com>
2025-02-23 15:33:58 +0500

    Fix parse info issue

--- library/src/main/java/com/farimarwat/library/mapper/VideoFormat.kt
+++ library/src/main/java/com/farimarwat/library/mapper/VideoFormat.kt
@@ -1,38 +1,26 @@
 package com.farimarwat.library
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
-
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class VideoFormat (
-    val asr:Int = 0,
-    val tbr:Int = 0,
-    val abr:Int = 0,
-    val format: String? = null,
-
-    @JsonProperty("format_id")
-    val formatId: String? = null,
-
-    @JsonProperty("format_note")
-    val formatNote: String? = null,
-    val ext: String? = null,
-    val preference:Int = 0,
-    val vcodec: String? = null,
-    val acodec: String? = null,
-    val width:Int = 0,
-    val height:Int = 0,
-
-    @JsonProperty("filesize")
-    val fileSize: Long = 0,
-
-    @JsonProperty("filesize_approx")
-    val fileSizeApproximate: Long = 0,
-    val fps:Int = 0,
-    val url: String? = null,
-
-    @JsonProperty("manifest_url")
-    val manifestUrl: String? = null,
-
-    @JsonProperty("http_headers")
-    val httpHeaders: Map<String, String>? = null,
+data class VideoFormat @JsonCreator constructor(
+    @JsonProperty("asr") val asr: Int = 0,
+    @JsonProperty("tbr") val tbr: Int = 0,
+    @JsonProperty("abr") val abr: Int = 0,
+    @JsonProperty("format") val format: String? = null,
+    @JsonProperty("format_id") val formatId: String? = null,
+    @JsonProperty("format_note") val formatNote: String? = null,
+    @JsonProperty("ext") val ext: String? = null,
+    @JsonProperty("preference") val preference: Int = 0,
+    @JsonProperty("vcodec") val vcodec: String? = null,
+    @JsonProperty("acodec") val acodec: String? = null,
+    @JsonProperty("width") val width: Int = 0,
+    @JsonProperty("height") val height: Int = 0,
+    @JsonProperty("filesize") val fileSize: Long = 0,
+    @JsonProperty("filesize_approx") val fileSizeApproximate: Long = 0,
+    @JsonProperty("fps") val fps: Int = 0,
+    @JsonProperty("url") val url: String? = null,
+    @JsonProperty("manifest_url") val manifestUrl: String? = null,
+    @JsonProperty("http_headers") val httpHeaders: Map<String, String>? = null,
 )
--- library/src/main/java/com/farimarwat/library/mapper/VideoInfo.kt
+++ library/src/main/java/com/farimarwat/library/mapper/VideoInfo.kt
@@ -1,84 +1,47 @@
 package com.farimarwat.library
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class VideoInfo (
-    val id: String? = null,
-    val fulltitle: String? = null,
-    val title: String? = null,
-
-    @JsonProperty("upload_date")
-    val uploadDate: String? = null,
-
-    @JsonProperty("display_id")
-    val displayId: String? = null,
-    val duration:Int = 0,
-    val description: String? = null,
-    val thumbnail: String? = null,
-    val license: String? = null,
-    val extractor: String? = null,
-
-    @JsonProperty("extractor_key")
-    val extractorKey: String? = null,
-
-    @JsonProperty("view_count")
-    val viewCount: String? = null,
-
-    @JsonProperty("like_count")
-    val likeCount: String? = null,
-
-    @JsonProperty("dislike_count")
-    val dislikeCount: String? = null,
-
-    @JsonProperty("repost_count")
-    val repostCount: String? = null,
-
-    @JsonProperty("average_rating")
-    val averageRating: String? = null,
-
-    @JsonProperty("uploader_id")
-    val uploaderId: String? = null,
-    val uploader: String? = null,
-
-    @JsonProperty("player_url")
-    val playerUrl: String? = null,
-
-    @JsonProperty("webpage_url")
-    val webpageUrl: String? = null,
-
-    @JsonProperty("webpage_url_basename")
-    val webpageUrlBasename: String? = null,
-    val resolution: String? = null,
-    val width:Int = 0,
-    val height:Int = 0,
-    val format: String? = null,
-
-    @JsonProperty("format_id")
-    val formatId: String? = null,
-    val ext: String? = null,
-
-    @JsonProperty("filesize")
-    val fileSize: Long = 0,
-
-    @JsonProperty("filesize_approx")
-    val fileSizeApproximate: Long = 0,
-
-    @JsonProperty("http_headers")
-    val httpHeaders: Map<String, String>? = null,
-    val categories: ArrayList<String>? = null,
-    val tags: ArrayList<String>? = null,
-
-    @JsonProperty("requested_formats")
-    val requestedFormats: ArrayList<VideoFormat>? = null,
-    val formats: ArrayList<VideoFormat>? = null,
-    val thumbnails: ArrayList<VideoThumbnail>? = null,
-
-    @JsonProperty("manifest_url")
-    val manifestUrl: String? = null,
-    val url: String? = null,
-
-    @JsonProperty("is_live")
-    val isLive:Boolean? = null,
+data class VideoInfo @JsonCreator constructor(
+    @JsonProperty("id") val id: String? = null,
+    @JsonProperty("fulltitle") val fulltitle: String? = null,
+    @JsonProperty("title") val title: String? = null,
+    @JsonProperty("upload_date") val uploadDate: String? = null,
+    @JsonProperty("display_id") val displayId: String? = null,
+    @JsonProperty("duration") val duration: Int = 0,
+    @JsonProperty("description") val description: String? = null,
+    @JsonProperty("thumbnail") val thumbnail: String? = null,
+    @JsonProperty("license") val license: String? = null,
+    @JsonProperty("extractor") val extractor: String? = null,
+    @JsonProperty("extractor_key") val extractorKey: String? = null,
+    @JsonProperty("view_count") val viewCount: String? = null,
+    @JsonProperty("like_count") val likeCount: String? = null,
+    @JsonProperty("dislike_count") val dislikeCount: String? = null,
+    @JsonProperty("repost_count") val repostCount: String? = null,
+    @JsonProperty("average_rating") val averageRating: String? = null,
+    @JsonProperty("uploader_id") val uploaderId: String? = null,
+    @JsonProperty("uploader") val uploader: String? = null,
+    @JsonProperty("player_url") val playerUrl: String? = null,
+    @JsonProperty("webpage_url") val webpageUrl: String? = null,
+    @JsonProperty("webpage_url_basename") val webpageUrlBasename: String? = null,
+    @JsonProperty("resolution") val resolution: String? = null,
+    @JsonProperty("width") val width: Int = 0,
+    @JsonProperty("height") val height: Int = 0,
+    @JsonProperty("format") val format: String? = null,
+    @JsonProperty("format_id") val formatId: String? = null,
+    @JsonProperty("ext") val ext: String? = null,
+    @JsonProperty("filesize") val fileSize: Long = 0,
+    @JsonProperty("filesize_approx") val fileSizeApproximate: Long = 0,
+    @JsonProperty("http_headers") val httpHeaders: Map<String, String>? = null,
+    @JsonProperty("categories") val categories: ArrayList<String>? = null,
+    @JsonProperty("tags") val tags: ArrayList<String>? = null,
+    @JsonProperty("requested_formats") val requestedFormats: ArrayList<VideoFormat>? = null,
+    @JsonProperty("formats") val formats: ArrayList<VideoFormat>? = null,
+    @JsonProperty("thumbnails") val thumbnails: ArrayList<VideoThumbnail>? = null,
+    @JsonProperty("manifest_url") val manifestUrl: String? = null,
+    @JsonProperty("url") val url: String? = null,
+    @JsonProperty("is_live") val isLive: Boolean? = null
 )
