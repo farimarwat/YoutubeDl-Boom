763d73324a9d50179090d9ca3cde4a3d208f303f
farimarwat <farimarwat@gmail.com>
2025-02-23 12:09:51 +0500

    Convert VideoInfo class to data class

--- library/src/main/java/com/farimarwat/library/mapper/VideoInfo.kt
+++ library/src/main/java/com/farimarwat/library/mapper/VideoInfo.kt
@@ -4,125 +4,81 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-class VideoInfo {
-    val id: String? = null
-    val fulltitle: String? = null
-    val title: String? = null
+data class VideoInfo (
+    val id: String? = null,
+    val fulltitle: String? = null,
+    val title: String? = null,
 
     @JsonProperty("upload_date")
-    val uploadDate: String? = null
+    val uploadDate: String? = null,
 
     @JsonProperty("display_id")
-    val displayId: String? = null
-    val duration = 0
-    val description: String? = null
-    val thumbnail: String? = null
-    val license: String? = null
-    val extractor: String? = null
+    val displayId: String? = null,
+    val duration:Int = 0,
+    val description: String? = null,
+    val thumbnail: String? = null,
+    val license: String? = null,
+    val extractor: String? = null,
 
     @JsonProperty("extractor_key")
-    val extractorKey: String? = null
+    val extractorKey: String? = null,
 
     @JsonProperty("view_count")
-    val viewCount: String? = null
+    val viewCount: String? = null,
 
     @JsonProperty("like_count")
-    val likeCount: String? = null
+    val likeCount: String? = null,
 
     @JsonProperty("dislike_count")
-    val dislikeCount: String? = null
+    val dislikeCount: String? = null,
 
     @JsonProperty("repost_count")
-    val repostCount: String? = null
+    val repostCount: String? = null,
 
     @JsonProperty("average_rating")
-    val averageRating: String? = null
+    val averageRating: String? = null,
 
     @JsonProperty("uploader_id")
-    val uploaderId: String? = null
-    val uploader: String? = null
+    val uploaderId: String? = null,
+    val uploader: String? = null,
 
     @JsonProperty("player_url")
-    val playerUrl: String? = null
+    val playerUrl: String? = null,
 
     @JsonProperty("webpage_url")
-    val webpageUrl: String? = null
+    val webpageUrl: String? = null,
 
     @JsonProperty("webpage_url_basename")
-    val webpageUrlBasename: String? = null
-    val resolution: String? = null
-    val width = 0
-    val height = 0
-    val format: String? = null
+    val webpageUrlBasename: String? = null,
+    val resolution: String? = null,
+    val width:Int = 0,
+    val height:Int = 0,
+    val format: String? = null,
 
     @JsonProperty("format_id")
-    val formatId: String? = null
-    val ext: String? = null
+    val formatId: String? = null,
+    val ext: String? = null,
 
     @JsonProperty("filesize")
-    val fileSize: Long = 0
+    val fileSize: Long = 0,
 
     @JsonProperty("filesize_approx")
-    val fileSizeApproximate: Long = 0
+    val fileSizeApproximate: Long = 0,
 
     @JsonProperty("http_headers")
-    val httpHeaders: Map<String, String>? = null
-    val categories: ArrayList<String>? = null
-    val tags: ArrayList<String>? = null
+    val httpHeaders: Map<String, String>? = null,
+    val categories: ArrayList<String>? = null,
+    val tags: ArrayList<String>? = null,
 
     @JsonProperty("requested_formats")
-    val requestedFormats: ArrayList<VideoFormat>? = null
-    val formats: ArrayList<VideoFormat>? = null
-    val thumbnails: ArrayList<VideoThumbnail>? = null
+    val requestedFormats: ArrayList<VideoFormat>? = null,
+    val formats: ArrayList<VideoFormat>? = null,
+    val thumbnails: ArrayList<VideoThumbnail>? = null,
 
     @JsonProperty("manifest_url")
-    val manifestUrl: String? = null
-    val url: String? = null
+    val manifestUrl: String? = null,
+    val url: String? = null,
 
     @JsonProperty("is_live")
-    val isLive:Boolean? = null
-
-
-    override fun toString(): String {
-        return """
-            VideoInfo(
-                id=$id,
-                title=$title,
-                fulltitle=$fulltitle,
-                uploadDate=$uploadDate,
-                displayId=$displayId,
-                duration=$duration,
-                description=$description,
-                thumbnail=$thumbnail,
-                license=$license,
-                extractor=$extractor,
-                extractorKey=$extractorKey,
-                viewCount=$viewCount,
-                likeCount=$likeCount,
-                dislikeCount=$dislikeCount,
-                repostCount=$repostCount,
-                averageRating=$averageRating,
-                uploaderId=$uploaderId,
-                uploader=$uploader,
-                playerUrl=$playerUrl,
-                webpageUrl=$webpageUrl,
-                resolution=$resolution,
-                width=$width,
-                height=$height,
-                format=$format,
-                formatId=$formatId,
-                ext=$ext,
-                fileSize=$fileSize,
-                fileSizeApproximate=$fileSizeApproximate,
-                httpHeaders=$httpHeaders,
-                categories=$categories,
-                tags=$tags,
-                requestedFormats=$requestedFormats,
-                formats=$formats,
-                thumbnails=$thumbnails,
-                manifestUrl=$manifestUrl,
-                url=$url
-            )
-        """.trimIndent()
-    }
-}
+    val isLive:Boolean? = null,
+)
